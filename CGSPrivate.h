efac07f0422cf74d6111785c6b6f2943446c90ff
Fjölnir Ásgeirsson <fjolnir@asgeirsson.is>
2012-11-22 02:52:12 +0900

    Added very limited support for spaces (hotkeys for jumping between them, and xnomad now recognizes if a window is not on the current space, and ignores it)

--- CGSPrivate.h
+++ CGSPrivate.h
@@ -36,3 +36,38 @@ extern OSStatus CGSGetScreenRectForWindow(const CGSConnection cid, CGSWindow wid
 extern OSStatus CGSGetWindowOwner(const CGSConnection cid, const CGSWindow wid, CGSConnection *ownerCid);
 extern OSStatus CGSConnectionGetPID(const CGSConnection cid, pid_t *pid, const CGSConnection ownerCid);
 extern OSStatus CGSGetConnectionIDForPSN(const CGSConnection cid, ProcessSerialNumber *psn, CGSConnection *out);
+
+#pragma mark - Spaces
+
+typedef uint64_t CGSSpace;
+typedef enum _CGSSpaceType {
+    kCGSSpaceUser,
+    kCGSSpaceFullscreen,
+    kCGSSpaceSystem,
+    kCGSSpaceUnknown
+} CGSSpaceType;
+typedef enum _CGSSpaceSelector {
+    kCGSSpaceCurrent = 5,
+    kCGSSpaceOther = 6,
+    kCGSSpaceAll = 7
+} CGSSpaceSelector;
+
+extern CFArrayRef CGSCopySpaces(const CGSConnection cid, CGSSpaceSelector type);
+extern CFArrayRef CGSCopySpacesForWindows(const CGSConnection cid, CGSSpaceSelector type, CFArrayRef windows);
+extern CGSSpaceType CGSSpaceGetType(const CGSConnection cid, CGSSpace space);
+
+extern CFNumberRef CGSWillSwitchSpaces(const CGSConnection cid, CFArrayRef a);
+extern void CGSHideSpaces(const CGSConnection cid, CFArrayRef spaces);
+extern void CGSShowSpaces(const CGSConnection cid, CFArrayRef spaces);
+
+extern void CGSAddWindowsToSpaces(const CGSConnection cid, CFArrayRef windows, CFArrayRef spaces);
+extern void CGSRemoveWindowsFromSpaces(const CGSConnection cid, CFArrayRef windows, CFArrayRef spaces);
+
+typedef uint64_t CGSManagedDisplay;
+extern CGSManagedDisplay kCGSPackagesMainDisplayIdentifier;
+
+extern void CGSManagedDisplaySetCurrentSpace(const CGSConnection cid, CGSManagedDisplay display, CGSSpace space);
+
+#pragma mark - Accessibility
+
+AXError _AXUIElementGetWindow(AXUIElementRef, CGWindowID* out);
