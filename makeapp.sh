02537c7985e417c86d803c6f7529bf074e5ccd9d
Fjölnir Ásgeirsson <fjolnir@asgeirsson.is>
2013-04-10 01:35:00 +0900

    Added a script to generate a .app bundle

---
+++ makeapp.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Requires Platypus's command line tool to be installed (http://sveinbjorn.org/platypus)
+platypus -a xnomad -p /usr/local/tranquil/bin/tranquil -V 0.1 -u Fjölnir\ Ásgeirsson -f Accessibility.tq -f Hotkeys.tq -f Layouts.tq -f WindowManager.tq -f CGSprivate.h -I is.fjolnir.xnomad -o None -B xnomad
